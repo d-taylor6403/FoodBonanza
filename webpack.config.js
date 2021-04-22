f95c831987cbb8042877735cf0aa0dd9a110a1e1
dannitay <dannielletaylor@outlook.com>
2021-04-21 20:26:01 -0500

    adding PWA functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
 
 module.exports = {
     entry: {
@@ -43,7 +45,22 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // The reports outputs to an HTML file in the dist folder
-        })
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+              src: path.resolve("assets/img/icons/icon-512x512.png"),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join("assets", "icons")
+            }]
+          })
     ],
     mode: 'development'
 };
